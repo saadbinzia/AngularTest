b04e587c51afba8725aec2c2f3a918b4fac22560
saad bin zia <saadbinzia5@gmail.com>
2022-04-18 12:51:09 +0500

    automated pr

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,11 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'routing-app';
   z = 100;
-  // z = 100;
-  // z = 100;
-  // z = 100;
-  // z = 100;
-  // z = 100;
+  z1 = 100;
+  z2 = 100;
+  z3 = 100;
+  z4 = 100;
+  z5 = 100;
 
   addToZ(x: any, y: any) {
     return x + y + this.z;
